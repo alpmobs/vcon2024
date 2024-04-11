6b0060feb8e72da09d3562eac7cb8778b4b6c84e
ARMUROPC\Cristina <alpmabeza@gmail.com>
2024-04-12 03:23:05 +0800

    Add smooth scrolling

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,8 +1,8 @@
-// add classes for mobile navigation toggling
 var CSbody = document.querySelector("body");
 const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
+// Add event listener to hamburger menu icon
 CShamburgerMenu.addEventListener("click", function () {
     CShamburgerMenu.classList.toggle("cs-active");
     CSnavbarMenu.classList.toggle("cs-active");
@@ -11,7 +11,19 @@ CShamburgerMenu.addEventListener("click", function () {
     ariaExpanded();
 });
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+// Add event listeners to navigation links to hide menu when clicked
+document.querySelectorAll('#cs-navigation a').forEach(link => {
+    link.addEventListener('click', function() {
+        // Hide navigation menu
+        CShamburgerMenu.classList.remove("cs-active");
+        CSnavbarMenu.classList.remove("cs-active");
+        CSbody.classList.remove("cs-open");
+        // Check the aria-expanded value
+        ariaExpanded();
+    });
+});
+
+// Check the value of aria-expanded on the cs-ul and change it accordingly whether it is expanded or not
 function ariaExpanded() {
     const csUL = document.querySelector("#cs-expanded");
     const csExpanded = csUL.getAttribute("aria-expanded");
